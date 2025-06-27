b0333f64bcbb21aaa1728f12fd0a3787cc773365
Victor Gerasimov <gvs1991.vs@gmail.com>
2025-06-27 12:13:59 +0300

    Connect styles and scripts together with bootstrap

--- homebook/settings.py
+++ homebook/settings.py
@@ -102,9 +102,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / STATIC_URL]
+# STATIC_ROOT = BASE_DIR / STATIC_URL
+
+# Media files
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
